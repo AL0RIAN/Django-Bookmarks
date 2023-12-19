19b2109b35fc287ac11cd4415630320797d0dbc6
Alorian <slowpok19450509@gmail.com>
2023-12-19 19:35:17 +0100

    Counting Image Views Using Redis Storage

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,13 +1,19 @@
-from django.http import HttpRequest, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from django.http import JsonResponse
+from django.http import HttpRequest, HttpResponse
 from django.views.decorators.http import require_POST
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from actions.utils import create_action
-from .models import Image
+from django.http import JsonResponse
+from django.contrib import messages
+from django.conf import settings
 from .forms import ImageCreateForm
+from .models import Image
+import redis
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 
 @login_required
@@ -33,11 +39,14 @@ def image_create(request: HttpRequest) -> HttpResponse:
 
 def image_detail(request: HttpRequest, id, slug) -> HttpResponse:
     image = get_object_or_404(Image, id=id, slug=slug)
-    context = {
-        'section': 'images',
-        'image': image,
-    }
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    total_views = r.incr(f'image:{image.id}:views')
+    r.zincrby('image_ranking', 1, image.id)
+
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image,
+                   'total_views': total_views})
 
 
 @login_required
@@ -84,3 +93,17 @@ def image_list(request: HttpRequest) -> HttpResponse:
                   'images/image/list.html',
                   {'section': 'images',
                    'images': images})
+
+
+@login_required
+def image_ranking(request: HttpRequest) -> HttpResponse:
+    image_ranking = r.zrange('image_ranking', 0, -1,
+                             desc=True)[:10]
+
+    image_ranking_ids = [int(id) for id in image_ranking]
+    most_viewed = list(Image.objects.filter(
+        id__in=image_ranking_ids))
+    # most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
